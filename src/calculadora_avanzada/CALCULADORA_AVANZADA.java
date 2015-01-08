4bf83f1471686035c115e2970f163e6b734c75c7
alexisoller <alexisolleraw@gmail.com>
2015-01-08 21:15:46 +0100

    Update CALCULADORA_AVANZADA.java

--- src/calculadora_avanzada/CALCULADORA_AVANZADA.java
+++ src/calculadora_avanzada/CALCULADORA_AVANZADA.java
@@ -18,6 +18,30 @@ public class CALCULADORA_AVANZADA {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Calculadora avanzada.");
+         int menu=6;
+         
+        switch (menu) {
+            case 1:  monthString = "January";
+                     break;
+            case 2:  monthString = "February";
+                     break;
+            case 3:  monthString = "March";
+                     break;
+            case 4:  monthString = "April";
+                     break;
+            case 5:  monthString = "May";
+                     break;
+            case 6:  monthString = "June";
+                     break;
+            case 7:  monthString = "July";
+                     break;
+            case 8:  monthString = "August";
+                     break;
+            default: monthString = "Invalid month";
+                     break;
+        }
+        
+        
         System.out.println("Elige una opcion: ");
     }
     
