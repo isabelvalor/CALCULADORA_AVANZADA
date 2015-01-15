b88d059fd5f5835d71ab475d5504a245abae96ea
alexisoller <alexisolleraw@gmail.com>
2015-01-15 20:22:05 +0100

    Update CALCULADORA_AVANZADA.java

--- src/calculadora_avanzada/CALCULADORA_AVANZADA.java
+++ src/calculadora_avanzada/CALCULADORA_AVANZADA.java
@@ -5,47 +5,64 @@
  */
 
 package calculadora_avanzada;
-
+import java.util.Scanner;
+import java.lang.Math ;
 /**
  *
  * @author vesprada
  */
  
 public class CALCULADORA_AVANZADA {
-import java.utils.Scanner
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner introducir=new Scanner(System.in); 
-     
+        int menu,x,y,i=0,resultado=0;
+        double resul=0;
         System.out.println("Calculadora avanzada.");
         System.out.println("Introducir ");
         System.out.println("Elige una opcion: ");
         menu=introducir.nextInt();
         
         switch (menu) {
-            case 1:  
-             
+            case 1: 
+                System.out.println("introduce el primer numero");
+                x=introducir.nextInt();
+                System.out.println("introduce el segundo numero");
+                y=introducir.nextInt();
+                resultado=x+y;
+                System.out.println("el sesultado es "+resultado);
                      break;
             case 2: 
-             
                      break;
             case 3: 
+                System.out.println("introduce el primer numero");
+                x=introducir.nextInt();
+                System.out.println("introduce el segundo numero");
+                y=introducir.nextInt();
+                resultado=x*y;
+                System.out.println("el sesultado es "+resultado);
+                
                      break;
             case 4:  
              
                      break;
             case 5: 
-           
+                System.out.println("introduce el numero");
+                x=introducir.nextInt();
+                System.out.println("introduce el exponente");
+                y=introducir.nextInt();
+                
+                resul=Math.pow(x,y);
+                
+                System.out.println("el sesultado es "+resultado);
+                
                      break;
             case 6:  
                      break;
-            case 7:  
-                     break;
-            case 8:  
-                     break;
-           
+            
         }
         
         
